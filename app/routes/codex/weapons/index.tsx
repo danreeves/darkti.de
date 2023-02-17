5af7fe00bc7db26473c6035bf5566e73c1e73bae
Dan Reeves <hey@danreev.es>
2023-02-17 02:54:36 +0000

    thats better

--- app/routes/codex/weapons/index.tsx
+++ app/routes/codex/weapons/index.tsx
@@ -59,7 +59,15 @@ function Checkbox({ value, name, label }: CheckboxProps) {
 	)
 }
 
-function TextInput({ label, name, className }: { label: string; name: string, className?: string }) {
+function TextInput({
+	label,
+	name,
+	className,
+}: {
+	label: string
+	name: string
+	className?: string
+}) {
 	const [searchParams] = useSearchParams()
 	const value = searchParams.get(name) ?? ""
 
@@ -94,7 +102,7 @@ function FormGroup({
 	children: ReactNode
 }) {
 	return (
-		<div className="flex flex-col [&:not(:first-child)]:ml-4">
+		<div className="flex flex-col ">
 			<div className="mx-2 font-heading text-xs font-bold uppercase">
 				{label}
 			</div>
@@ -115,7 +123,7 @@ export default function Weapons() {
 				ref={formRef}
 				method="get"
 				onChange={(e) => submit(e.currentTarget)}
-				className="mb-2 flex flex-row items-center"
+				className="mb-2 flex flex-row items-center gap-4"
 			>
 				<FormGroup label="Item type">
 					<Checkbox name="type" value="melee" label="Melee" />
@@ -129,24 +137,26 @@ export default function Weapons() {
 					<Checkbox name="archetype" value="ogryn" label="Ogryn" />
 				</FormGroup>
 
-				<TextInput label="Search" name="name" className="items-end ml-auto" />
-
+				<TextInput label="Search" name="name" className="ml-auto items-end" />
 			</Form>
-			<ul className="grid md:grid-cols-2 grid-cols-1">
+			<ul className="grid grid-cols-1 md:grid-cols-2">
 				{weapons.map((weapon) => {
 					return (
 						<li
 							className=" group relative m-2 overflow-hidden rounded bg-white shadow hover:shadow-lg"
 							key={weapon.id}
 						>
-							<div className="pointer-events-none absolute top-0 right-0 aspect-video h-full overflow-hidden z-0 ">
+							<div className="pointer-events-none absolute top-0 right-0 z-0 aspect-video h-full overflow-hidden ">
 								<img
 									alt=""
 									src={`https://img.darkti.de/pngs/${weapon.preview_image}.png`}
 									className="h-full transition duration-75 group-hover:scale-105"
 								/>
 							</div>
-							<Link to={weapon.slug} className="block h-full w-full p-4 z-10 isolate">
+							<Link
+								to={weapon.slug}
+								className="isolate z-10 block h-full w-full p-4"
+							>
 								<div className="mb-2 text-lg font-bold">
 									{weapon.display_name}
 								</div>
@@ -158,10 +168,11 @@ export default function Weapons() {
 				})}
 			</ul>
 			{weapons.length < 1 ? (
-
 				<div className="px-4 py-6 sm:px-0">
-					<div className="h-96 rounded-lg border-4 border-dashed border-gray-200 grid place-content-center">
-						<span className="font-heading font-black text-lg text-neutral-400">No results</span>
+					<div className="grid h-96 place-content-center rounded-lg border-4 border-dashed border-gray-200">
+						<span className="font-heading text-lg font-black text-neutral-400">
+							No results
+						</span>
 					</div>
 				</div>
 			) : null}
