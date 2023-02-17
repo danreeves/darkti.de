379e33503ca10a5f003005dc51113ee77143a873
Dan Reeves <hey@danreev.es>
2023-02-17 17:11:12 +0000

    try with custom headers

--- app/routes/api/event.ts
+++ app/routes/api/event.ts
@@ -1,9 +1,27 @@
-import type { ActionArgs } from "@remix-run/node"; // or cloudflare/deno
+import type { ActionArgs } from "@remix-run/node" // or cloudflare/deno
+import { getClientIPAddress } from "remix-utils"
 
 export const action = async ({ request }: ActionArgs) => {
-	const { origin } = new URL(request.url)
-	const analyticsHost = 'https://plausible.io'
-	const forwardPath = request.url.replace(origin, analyticsHost)
+  const { origin } = new URL(request.url)
+  const analyticsHost = "https://plausible.io"
+  const forwardPath = request.url.replace(origin, analyticsHost)
 
-	return await fetch(forwardPath, { method: request.method, headers: request.headers, body: request.body })
-};
+  const clientIp = getClientIPAddress(request)
+  const clientUserAgent = request.headers.get("user-agent")
+
+  if (!clientIp || !clientUserAgent) {
+    return
+  }
+
+  const headers = {
+    "content-type": "application/json",
+    "x-forwarded-for": clientIp,
+    "user-agent": clientUserAgent,
+  }
+
+  return await fetch(forwardPath, {
+    method: request.method,
+    headers,
+    body: request.body,
+  })
+}
