7754fd974a1ae3d6353f7686fa978825fcdafba7
Dan Reeves <hey@danreev.es>
2023-02-19 02:24:51 +0000

    share form components, add search to skins

---
+++ app/components/Form.tsx
@@ -0,0 +1,100 @@
+import { useSearchParams, useSubmit } from "@remix-run/react"
+import type { ReactNode } from "react"
+import { Form as RemixForm } from "@remix-run/react"
+
+type CheckboxProps = {
+	value: string
+	name: string
+	label: string
+}
+export function Checkbox({ value, name, label }: CheckboxProps) {
+	const [searchParams] = useSearchParams()
+	const param = searchParams.getAll(name)
+
+	return (
+		<label htmlFor={value} className="m-2 inline-flex items-center">
+			<input
+				type="checkbox"
+				id={value}
+				name={name}
+				value={value}
+				defaultChecked={param.includes(value)}
+				className="mr-2
+                          rounded
+                          border-transparent
+                          bg-gray-200 text-gray-700
+                          focus:border-transparent
+                          focus:bg-gray-200 focus:ring-1 focus:ring-gray-500
+						  focus:ring-offset-2
+                        "
+			/>
+			{label}
+		</label>
+	)
+}
+
+export function TextInput({
+	label,
+	name,
+	className,
+}: {
+	label: string
+	name: string
+	className?: string
+}) {
+	const [searchParams] = useSearchParams()
+	const value = searchParams.get(name) ?? ""
+
+	return (
+		<label htmlFor={name} className={`flex flex-col ${className}`}>
+			<span className="font-heading text-xs font-black uppercase">{label}</span>
+			<input
+				type="text"
+				name={name}
+				id={name}
+				defaultValue={value}
+				className="
+                    mt-1
+                    block
+                    w-full
+                    rounded-md
+                    border-gray-300
+					border-transparent
+                    bg-white
+                    focus:border-gray-500 focus:bg-white focus:ring-0
+                  "
+			/>
+		</label>
+	)
+}
+
+export function FormGroup({
+	label,
+	children,
+}: {
+	label: string
+	children: ReactNode
+}) {
+	return (
+		<div className="flex flex-col ">
+			<div className="mx-2 font-heading text-xs font-bold uppercase">
+				{label}
+			</div>
+			<div>{children}</div>
+		</div>
+	)
+}
+
+export function Form({ children }: { children: ReactNode }) {
+	const submit = useSubmit()
+
+	return (
+		<RemixForm
+			method="get"
+			onChange={(e) => submit(e.currentTarget)}
+			className="mb-2 flex flex-row items-center gap-4"
+		>
+			{children}
+		</RemixForm>
+	)
+}
--- app/routes/codex/skins/index.tsx
+++ app/routes/codex/skins/index.tsx
@@ -1,48 +1,55 @@
 import { json } from "@remix-run/node"
+import type { LoaderArgs } from "@remix-run/node"
 import { Link, useLoaderData } from "@remix-run/react"
+import { Form, TextInput } from "~/components/Form"
 import { getItems } from "~/data/items.server"
 import { SkinSchema } from "~/data/schemas.server"
 
-export const loader = async () => {
-  const items = await getItems(SkinSchema)
-  return json({ title: "Skins", items })
+export const loader = async ({ request }: LoaderArgs) => {
+	const url = new URL(request.url)
+	const name = url.searchParams.get("name") ?? undefined
+	const items = await getItems(SkinSchema, { name })
+	return json({ title: "Skins", items })
 }
 
 export default function Skins() {
-  const { items } = useLoaderData<typeof loader>()
+	const { items } = useLoaderData<typeof loader>()
 
-  return (
-    <>
-      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-        {items.map((item) => {
-          return (
-            <li
-              className=" group relative m-2 overflow-hidden rounded bg-white shadow hover:shadow-lg"
-              key={item.id}
-            >
-              <Link to={item.slug} className="block h-full w-full">
-                <div className="aspect-video w-full overflow-hidden">
-                  <img
-                    alt=""
-                    src={`https://img.darkti.de/pngs/${item.preview_image}.png`}
-                    className="h-full transition duration-75 group-hover:scale-105"
-                  />
-                </div>
-                <div className="p-4 text-lg font-bold">{item.display_name}</div>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-      {items.length < 1 ? (
-        <div className="px-4 py-6 sm:px-0">
-          <div className="grid h-96 place-content-center rounded-lg border-4 border-dashed border-gray-200">
-            <span className="font-heading text-lg font-black text-neutral-400">
-              No results
-            </span>
-          </div>
-        </div>
-      ) : null}
-    </>
-  )
+	return (
+		<>
+			<Form>
+				<TextInput label="Search" name="name" className="ml-auto items-end" />
+			</Form>
+			<ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+				{items.map((item) => {
+					return (
+						<li
+							className=" group relative m-2 overflow-hidden rounded bg-white shadow hover:shadow-lg"
+							key={item.id}
+						>
+							<Link to={item.slug} className="block h-full w-full">
+								<div className="aspect-video w-full overflow-hidden">
+									<img
+										alt=""
+										src={`https://img.darkti.de/pngs/${item.preview_image}.png`}
+										className="h-full transition duration-75 group-hover:scale-105"
+									/>
+								</div>
+								<div className="p-4 text-lg font-bold">{item.display_name}</div>
+							</Link>
+						</li>
+					)
+				})}
+			</ul>
+			{items.length < 1 ? (
+				<div className="px-4 py-6 sm:px-0">
+					<div className="grid h-96 place-content-center rounded-lg border-4 border-dashed border-gray-200">
+						<span className="font-heading text-lg font-black text-neutral-400">
+							No results
+						</span>
+					</div>
+				</div>
+			) : null}
+		</>
+	)
 }
