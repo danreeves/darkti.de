babcd9da0a4e7a8ac8d63d20afe4bef384192345
Dan Reeves <hey@danreev.es>
2023-02-26 11:23:29 +0000

    add some links to modding page

--- app/routes/modding.tsx
+++ app/routes/modding.tsx
@@ -9,7 +9,15 @@ export default function Modding() {
 		<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
 			<div className="rounded bg-white p-4 shadow">
 				<h2 className="mb-2 font-heading text-lg">Using mods</h2>
-				<p className="italic text-gray-400">Coming soon...</p>
+				<p>
+					<a href="https://discord.gg/GFyCGNpJb8">Modders discord</a>
+				</p>
+				<p>
+					<a href="https://www.nexusmods.com/warhammer40kdarktide">Nexus Mods</a>
+				</p>
+				<p>
+					<a href="https://github.com/danreeves/vortex-extension-darktide">Vortex Mod Loader Extension</a>
+				</p>
 			</div>
 			<div className="rounded bg-white p-4 shadow">
 				<h2 className="mb-2 font-heading text-lg">Making mods</h2>
