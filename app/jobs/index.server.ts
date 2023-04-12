ccb8c1882ef8981b8ad5b39e4f5335a6dc03077c
Dan Reeves <hey@danreev.es>
2023-04-12 23:38:52 +0100

    refresh sooner

--- app/jobs/index.server.ts
+++ app/jobs/index.server.ts
@@ -9,7 +9,7 @@ export function initJobs() {
   global.tokenRefresher =
     global.tokenRefresher ??
     Cron("?/5 * * * *", { protect: true }, async () => {
-      let expiringInTheNext10Minutes = 10
+      let expiringInTheNext10Minutes = 30
       await refreshTokens(expiringInTheNext10Minutes)
     })
 }
