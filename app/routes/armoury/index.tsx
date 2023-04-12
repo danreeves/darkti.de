67e89e58d2634184a92e60a8af7d3d5dd4c57732
Dan Reeves <hey@danreev.es>
2023-04-12 22:32:53 +0100

    return something

--- app/routes/armoury/index.tsx
+++ app/routes/armoury/index.tsx
@@ -5,18 +5,18 @@ import { authenticator } from "~/services/auth.server"
 import { getAccountSummary } from "~/services/darktide.server"
 
 export let loader = async ({ request }: LoaderArgs) => {
-	let user = await authenticator.isAuthenticated(request, {
-		failureRedirect: "/login",
-	})
+  let user = await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  })
 
-	let auth = await getAuthToken(user.id)
-	if (auth) {
-		let account = await getAccountSummary(auth)
-		let firstCharId = account?.summary.characters[0].id
-		if (firstCharId) {
-			return redirect(`/armoury/${firstCharId}/inventory`)
-		}
-	}
+  let auth = await getAuthToken(user.id)
+  if (auth) {
+    let account = await getAccountSummary(auth)
+    let firstCharId = account?.summary.characters[0].id
+    if (firstCharId) {
+      return redirect(`/armoury/${firstCharId}/inventory`)
+    }
+  }
 
-	// return redirect("/login")
+  return null
 }
