eaace6ef6549984e484cc1885e91912d4e8dcb7a
Dan Reeves <hey@danreev.es>
2023-04-13 01:17:32 +0100

    tidy some logs

--- app/routes/auth/steam/$steamId.ts
+++ app/routes/auth/steam/$steamId.ts
@@ -6,53 +6,50 @@ import { checkToken, joinQueue } from "~/services/darktide.server"
 import { sleep } from "~/utils/sleep"
 
 export let loader = async ({ request, params }: LoaderArgs) => {
-	let steamSessionTicket = request.headers.get("steam-auth-session-ticket")
-	if (!params.steamId || !steamSessionTicket) {
-		return json({ error: "Invalid request" }, { status: 400 })
-	}
+  let steamSessionTicket = request.headers.get("steam-auth-session-ticket")
+  if (!params.steamId || !steamSessionTicket) {
+    return json({ error: "Invalid request" }, { status: 400 })
+  }
 
-	let user = await getUserBySteamId(params.steamId)
-	if (!user) {
-		return json(
-			{ error: "User not found. Sign up at https://darkti.de first." },
-			{ status: 404 }
-		)
-	}
+  let user = await getUserBySteamId(params.steamId)
+  if (!user) {
+    return json(
+      { error: "User not found. Sign up at https://darkti.de first." },
+      { status: 404 }
+    )
+  }
 
-	let join = await joinQueue(steamSessionTicket)
+  let join = await joinQueue(steamSessionTicket)
 
-	if (!join) {
-		return json({ error: "Failed to join auth queue" }, { status: 400 })
-	}
+  if (!join) {
+    return json({ error: "Failed to join auth queue" }, { status: 400 })
+  }
 
-	let auth = await checkToken(join.queueTicket)
+  let auth = await checkToken(join.queueTicket)
 
-	let checks = 0
-	let maxChecks = join.queuePosition + 1
-	while (!auth && checks <= maxChecks) {
-		console.log("Checking auth queue", checks, "times")
-		await sleep(1000)
-		auth = await checkToken(join.queueTicket)
-		checks++
-	}
+  let checks = 0
+  let maxChecks = join.queuePosition + 1
+  while (!auth && checks <= maxChecks) {
+    console.log("Checking auth queue", checks, "times")
+    await sleep(1000)
+    auth = await checkToken(join.queueTicket)
+    checks++
+  }
 
-	if (!auth) {
-		return json({ error: "Failed to get auth token" }, { status: 400 })
-	}
+  if (!auth) {
+    return json({ error: "Failed to get auth token" }, { status: 400 })
+  }
 
-	let expiresAt = new Date()
-	expiresAt.setSeconds(expiresAt.getSeconds() + auth.ExpiresIn)
+  let expiresAt = new Date()
+  expiresAt.setSeconds(expiresAt.getSeconds() + auth.ExpiresIn)
 
-	console.log(new Date())
-	console.log(expiresAt)
+  await updateAuthToken({
+    userId: user.id,
+    expiresAt: expiresAt,
+    sub: auth.Sub,
+    accessToken: auth.AccessToken,
+    refreshToken: auth.RefreshToken,
+  })
 
-	await updateAuthToken({
-		userId: user.id,
-		expiresAt: expiresAt,
-		sub: auth.Sub,
-		accessToken: auth.AccessToken,
-		refreshToken: auth.RefreshToken,
-	})
-
-	return json({ ok: true })
+  return json({ ok: true })
 }
