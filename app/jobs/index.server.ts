65fdd236a591ed2f07cbf08cc8ebf3f804256c31
Dan Reeves <hey@danreev.es>
2023-04-13 02:36:15 +0100

    reduce refresh time again

--- app/jobs/index.server.ts
+++ app/jobs/index.server.ts
@@ -9,8 +9,8 @@ export function initJobs() {
   global.tokenRefresher =
     global.tokenRefresher ??
     Cron("*/5 * * * *", async () => {
-      let expiringInTheNext15Minutes = 15
-      await refreshTokens(expiringInTheNext15Minutes)
+      let expiringInTheNext10Minutes = 10
+      await refreshTokens(expiringInTheNext10Minutes)
       console.log("Next refresh at", global.tokenRefresher.nextRun())
       return true
     })
