535bdf8426ea9558a1d55b58bc4c90e9ff8ab330
Ben <Lai>
2023-04-17 12:33:48 +1000

    updates

--- app/routes/armoury/$character/exchange.tsx
+++ app/routes/armoury/$character/exchange.tsx
@@ -1,11 +1,10 @@
 import { useLoaderData } from "@remix-run/react"
-import type { LoaderArgs } from "@remix-run/server-runtime"
+import { LoaderArgs, redirect } from "@remix-run/server-runtime"
 import { json } from "@remix-run/server-runtime"
 import { z } from "zod"
 import { zx } from "zodix"
 import { getAuthToken } from "~/data/authtoken.server"
 import { getItems } from "~/data/items.server"
-import { t } from "~/data/localization.server"
 import { PerkSchema, WeaponSchema } from "~/data/schemas.server"
 import { replaceAll } from "~/data/utils.server"
 import { authenticator } from "~/services/auth.server"
@@ -26,14 +25,18 @@ export async function loader({ request, params }: LoaderArgs) {
 
 	if (auth) {
 		let characterList = await getCharacters(auth)
+    if(characterList === undefined) {
+      redirect("/armoury")    
+      return json({ offers:[] })
+    }
 		let currentCharacter =
-			characterList?.characters[
-				characterList?.characters.findIndex((c) => c.id == character)
+			characterList.characters[
+				characterList.characters.findIndex((c) => c.id == character)
 			]
 		let currentShop = await getCharacterStore(
 			auth,
-			currentCharacter?.archetype!,
-			currentCharacter?.id!
+			currentCharacter.archetype,
+			currentCharacter.id
 		)
 		const weapons = await getItems(WeaponSchema)
     const traits = await getItems(PerkSchema)
@@ -43,13 +46,18 @@ export async function loader({ request, params }: LoaderArgs) {
 			.map(([id, item]) => {
 				let weapon = weapons.find((wep) => wep.id === item?.description.id)
 				if (!weapon) return undefined
-        item?.description.overrides.perks?.map(perk => {
-          let trait = traits.find((trait) => trait.id === perk.id)
-          let values = trait?.description_values?.filter((value) => +value.rarity === perk.rarity)
-          let replacement : {[key : string]: string} = {}
-          values?.map((value) => replacement["{" + value.string_key + ":%s}"] = value.string_value)
-          perk.id =  replaceAll(trait?.description!, replacement)
-        })
+        if (!item) return undefined
+        if(item.description.overrides.perks){
+          item.description.overrides.perks.forEach(perk => {
+            let trait = traits.find((trait) => trait.id === perk.id)
+            if(trait && trait.description && trait.description_values){
+              let values = trait.description_values.filter((value) => +value.rarity === perk.rarity)
+              let replacement : {[key : string]: string} = {}
+              values.map((value) => replacement["{" + value.string_key + ":%s}"] = value.string_value)
+              perk.id =  replaceAll(trait.description, replacement)
+            }
+          })
+        }
 				return {
 					id,
 					item,
@@ -58,7 +66,7 @@ export async function loader({ request, params }: LoaderArgs) {
 			})
 			.filter(Boolean)
 		return json({ offers: offers })
-	}
+    }
 	return json({ offers: []})
 }
 
@@ -74,29 +82,33 @@ export default function Exchange() {
 	let { offers } = useLoaderData<typeof loader>()
 	return (
 		<div className="grid w-full grow grid-cols-4 gap-4 bg-neutral-200 p-4 shadow-inner">
-			{offers.map((item : any) => (
+			{offers.map((offer) => (
 				<div
-					key={item.id}
+					key={offer.id}
 					className={classnames(
 						"border-l-3 relative border-2 border-neutral-400 bg-white p-4 shadow",
-						rarityBorder[String(item.item.description.overrides.rarity) ?? "0"]
+						rarityBorder[String(offer.item.description.overrides.rarity) ?? "0"]
 					)}
 				>
-					<div>{item.weapon.display_name}</div>
+					<div>{offer.weapon.display_name}</div>
 					<div className="">
 						<div>Rating</div>
-						<span>{item.item.description.overrides.itemLevel}</span>
+						<span>{offer.item.description.overrides.itemLevel}</span>
+					</div>
+					<div className="">
+						<div>Base Rating</div>
+						<span>{offer.item.description.overrides.baseItemLevel}</span>
 					</div>
 					<div>
-						<div>{item.item.price.amount.type}</div>
+						<div>{offer.item.price.amount.type}</div>
 						<span style={{ color: "gold" }}>
-							{item.item.price.amount.amount}
+							{offer.item.price.amount.amount}
 						</span>
 					</div>
-					{item.item.description.overrides.perks.length > 0 ? (
+					{offer.item.description.overrides.perks && offer.item.description.overrides.perks.length > 0 ? (
 						<div>
 							<div>Perks</div>
-							{item.item.description.overrides.perks.map((perk: {id: string, rarity:number}) => (
+							{offer.item.description.overrides.perks.map((perk) => (
 								<div key={perk.id}>{perk.id}</div>
 							))}
 							<span></span>
@@ -107,7 +119,7 @@ export default function Exchange() {
 						alt=""
 						loading="lazy"
 						className="max-w-1/2 pointer-events-none absolute right-0 top-0 h-full"
-						src={`https://img.darkti.de/pngs/${item.weapon.preview_image}.png`}
+						src={`https://img.darkti.de/pngs/${offer.weapon.preview_image}.png`}
 					/>
 				</div>
 			))}
