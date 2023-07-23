f59f176ec04199b3979f2e3cbc3a38fbb41153da
Dan Reeves <hey@danreev.es>
2023-07-23 15:09:48 +0100

    migrate to hono

---
+++ remix.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+	future: {
+		v2_dev: true,
+		// TODO:
+		// v2_headers: true,
+		// v2_errorBoundary: true,
+		// v2_meta: true,
+		// v2_normalizeFormMethod: true,
+		// v2_routeConvention: true,
+	},
+	tailwind: true,
+
+	server: "./server/server.ts",
+	serverDependenciesToBundle: "all",
+  serverBuildPath: "build/server.js",
+  serverMainFields: ["main", "module"],
+  serverMinify: false,
+  serverModuleFormat: "cjs",
+  serverPlatform: "node",
+
+	watchPaths: ["./server/**/*.ts"],
+
+	ignoredRouteFiles: ["**/.*", "**/__tests__/**"],
+}
