a5dd7ea44edd8d2bfeed1675387846a4eb5fd2ba
Dan Reeves <hey@danreev.es>
2023-10-12 01:09:24 +0100

    Update inventory styles

--- app/routes/armoury.$character.tsx
+++ app/routes/armoury.$character.tsx
@@ -1,9 +1,5 @@
 import { Outlet } from "@remix-run/react"
 
 export default function Character() {
-	return (
-		<div className="flex h-full flex-row overflow-hidden">
-			<Outlet />
-		</div>
-	)
+	return <Outlet />
 }
