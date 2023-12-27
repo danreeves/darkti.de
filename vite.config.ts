87b9fe214fa0f589f3987f525d143532c4c1a988
Dan Reeves <hey@danreev.es>
2023-12-27 22:13:04 +0000

    add sha and build time to footer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { execSync } from "child_process"
 import { unstable_vitePlugin as remix } from "@remix-run/dev"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
 	plugins: [remix(), tsconfigPaths()],
+	define: {
+		BUILD_TIME: JSON.stringify(new Date().toLocaleString("en-GB")),
+		GIT_SHA: JSON.stringify(
+			execSync("git rev-parse --short HEAD").toString().trim(),
+		),
+	},
 })
