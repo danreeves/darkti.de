dc3cd2e6da20a47b53323c33355e784e6b417d60
Dan Reeves <hey@danreev.es>
2025-08-17 16:43:24 +0100

    favicon

--- app/layouts/site.tsx
+++ app/layouts/site.tsx
@@ -7,19 +7,23 @@ export default function SiteLayout({ matches }: Route.ComponentProps) {
 	// @ts-expect-error -- We don't know the type of loaderData for every page but we expect a page title
 	const pageTitle = currentRoute?.loaderData?.title || "[UNKNOWN LOCATION]"
 	return (
-		<div className="container mx-auto min-h-screen p-6">
-			<div className="mb-4 flex flex-row items-center border-b border-green-500 pb-4">
-				<Book className="" />
-				<h1 className="font-machine flex-1 text-center text-5xl font-extrabold tracking-widest text-green-500">
-					+++ {pageTitle} +++
-				</h1>
+		<>
+			<title>{pageTitle}</title>
+			<link rel="icon" href="/favicon.png" />
+			<div className="container mx-auto min-h-screen p-6">
+				<div className="mb-4 flex flex-row items-center border-b border-green-500 pb-4">
+					<Book className="" />
+					<h1 className="font-machine flex-1 text-center text-5xl font-extrabold tracking-widest text-green-500">
+						+++ {pageTitle} +++
+					</h1>
+				</div>
+
+				<Outlet />
+
+				<footer className="mt-8 text-center text-sm text-green-600 uppercase">
+					<p>++++ For the Emperor ++++</p>
+				</footer>
 			</div>
-
-			<Outlet />
-
-			<footer className="mt-8 text-center text-sm text-green-600 uppercase">
-				<p>++++ For the Emperor ++++</p>
-			</footer>
-		</div>
+		</>
 	)
 }
